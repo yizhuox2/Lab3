ca81e4879a3f58e084b7364cdfde026a7efe61af
yizhuox2 <yizhuox2@illinois.edu>
2018-09-19 18:48:26 -0500

    Initial project commit.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
-public class Main {
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("HERE");
     }
-}
+/**
+ * Retrieve contents from a URL and return them as a string.
+ *
+ * @param url url to retrieve contents from
+ * @return the contents from the url as a string, or an empty string on error
+ */
+public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+        urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+        return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+        }}
